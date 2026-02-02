7172f12eda9965a583046fa1d7876fdb550c700f
PiyushKoranne <pkoranne.businessmail@gmail.com>
2026-02-02 23:52:58 +0530

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, } from 'react';
+import { useRef, useState, } from 'react';
 import type {ReactElement} from "react";
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
